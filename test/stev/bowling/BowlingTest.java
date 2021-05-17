abee78dd8130ace31e08e6e15e06c63be6bf4691
Matthieu Planas <mplanas78@gmail.com>
2021-05-17 15:51:04 +0200

    Initial commit

---
+++ test/stev/bowling/BowlingTest.java
@@ -0,0 +1,24 @@
+package stev.bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BowlingTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
